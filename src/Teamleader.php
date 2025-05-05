9500c9df286d5e314d5d6e6fb2614f2ea88acb63
Stefanie Gevaert <stefanie@koeketienedesign.be>
2025-05-05 16:16:10 +0200

    feat: added quotation field and updated the product selector

--- src/Teamleader.php
+++ src/Teamleader.php
@@ -11,6 +11,8 @@
 namespace craftpulse\teamleader;
 
 use Craft;
+use craft\services\Fields;
+use craftpulse\teamleader\fields\QuotationField;
 use Monolog\Formatter\LineFormatter;
 use Psr\Log\LogLevel;
 use Throwable;
@@ -162,6 +164,9 @@ class Teamleader extends Plugin
 
             // Elements
             $this->_registerElements();
+
+            // Fields
+            $this->_registerFields();
         }
 
 
@@ -371,6 +376,22 @@ class Teamleader extends Plugin
                 $event->types[] = Contact::class;
                 $event->types[] = Deal::class;
                 $event->types[] = Quotation::class;
+            }
+        );
+    }
+
+    /**
+     * Registers our custom fields
+     *
+     * @return void
+     */
+    private function _registerFields(): void
+    {
+        Event::on(
+            Fields::class,
+            Fields::EVENT_REGISTER_FIELD_TYPES,
+            function (RegisterComponentTypesEvent $event) {
+                $event->types[] = QuotationField::class;
             }
         );
     }
