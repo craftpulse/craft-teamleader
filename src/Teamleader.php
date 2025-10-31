4873359f2a732398cb06a33fb58487a66aa49cce
Adrienne L. Travis <adrienne@utilitarienne.com>
2025-10-31 00:54:26 -0700

    temp(authorization): restoring plus+pro check

--- src/Teamleader.php
+++ src/Teamleader.php
@@ -147,7 +147,7 @@ class Teamleader extends Plugin
             $this->_registerFormieEventHandlers();
         }
 
-        if (true /* $this->getIsPlus() || $this->getIsPro() */) {
+        if ($this->getIsPlus() || $this->getIsPro()) {
             $this->hasCpSettings = true;
             $this->hasCpSection = true;
 
